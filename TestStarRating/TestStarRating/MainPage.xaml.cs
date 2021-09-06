bedf8a6ee3f811d84ded49deed237032d3a7e5f7
주명 장 <jmjang@stis.co.kr>
2021-09-06 10:00:08 +0900

    초기 커밋.

---
+++ TestStarRating/TestStarRating/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestStarRating.ViewModel;
+using Xamarin.Forms;
+
+namespace TestStarRating
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new RattingBarViewModal();
+        }
+    }
+}
